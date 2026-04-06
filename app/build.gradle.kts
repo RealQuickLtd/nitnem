bcb0c4f594bcd17e9918895cf2d425b1acb9b6df
Quick ⚡ <quick@disroot.org>
2026-04-07 05:15:21 +0530

    release 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ltd.realquick.nitnem"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
         vectorDrawables.useSupportLibrary = true
     }
 
